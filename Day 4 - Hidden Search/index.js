74713632a36b720a3390a5eabffbe26bd764dd7e
shavla <rakviashvili95@gmail.com>
2023-04-08 19:49:03 +0400

    day 4

---
+++ Day 4 - Hidden Search/index.js
@@ -0,0 +1,7 @@
+"use strict";
+const btn = document.querySelector(".btn");
+const search = document.querySelector(".search");
+
+btn.addEventListener("click", () => {
+  search.classList.toggle("active");
+});
